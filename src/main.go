fd42303df4c65ad2dab1993658dd288386be3a20
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2022-02-12 21:01:53 -0600

    Hola mundo message

---
+++ src/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hola mundo")
+
+}
+
+// To ejecute this file you need to compile it in the terminal, we have two options:
+//  1. the fast: go run src/main.go
+//  2. the "hard" but with better performance:
+// 	2.1 go build src/main.go
+// 	2.2 ./main
