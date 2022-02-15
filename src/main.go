e2157cfa9f4cc2df159a61c5e3c6b3c2091d6602
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2022-02-15 12:07:08 -0600

    Como se usa un ciclo fo en go

--- src/main.go
+++ src/main.go
@@ -4,7 +4,24 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Hola mundo")
+	// For condicinal
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	// For while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
+
+	// For forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+		counterForever++
+	}
 
 }
 
