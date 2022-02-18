e14845e5b158d44f399e3baf7e50adec68fa11d2
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2022-02-17 18:18:44 -0600

    Como funciona el condicional if

--- src/main.go
+++ src/main.go
@@ -1,34 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func esPar(value int) bool {
+	if module := value % 2; module == 0 {
+		return true
+	}
+	return false
+}
+
+func getLogin(username, password string) {
+	/* Esta función compara el username y el password y verificar si coincide
+	   hacer login de esta manera (username y password en el código), es una muy mala práctica
+	   usualmente los frameworks utilizan librerías para hacer este check
+	   con passwords encriptados desde la base de datos.
+	*/
+	if username == "platzi" && password == "platzi" {
+		fmt.Println("You are logged")
+	} else {
+		fmt.Println("Username or password incorrect")
+	}
+}
 
 func main() {
+	valor1 := 1
+	valor2 := 2
 
-	// For condicinal
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	if valor1 == 1 {
+		fmt.Println("Es 1")
+	} else {
+		fmt.Println("No es 1")
 	}
 
-	// For while
-	counter := 0
-	for counter < 10 {
-		fmt.Println(counter)
-		counter++
+	// With and
+	if valor1 == 1 && valor2 == 3 {
+		fmt.Println("Es verdad")
 	}
 
-	// For forever
-	// counterForever := 0
-	// for {
-	// 	fmt.Println(counterForever)
-	// 	counterForever++
-	// }
-
-	// Reverse for
-	reverseCount := 10
-	for reverseCount >= 0 {
-		fmt.Println(reverseCount)
-		reverseCount--
+	// With or
+	if valor1 == 0 || valor2 == 2 {
+		fmt.Println("Es verdad, OR")
 	}
+
+	// Convertir texto a número
+	value, err := strconv.Atoi("53")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Value:", value)
+
+	// Evaluar número par e impar
+	fmt.Println(esPar(2))
+	fmt.Println(esPar(3))
+
+	// Función para hacer login
+	username := "platzi"
+	password := ""
+	getLogin(username, password)
 
 }
 
