471b6d3072d8afece567b382696381d78339f431
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2022-02-17 18:05:30 -0600

    Diferencia entre arrays y slices

--- src/main.go
+++ src/main.go
@@ -1,32 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func isPalindrome(text string) {
-	var textReverse string
-
-	text = strings.ToLower(text)
-
-	for i := len(text) - 1; i >= 0; i-- {
-		textReverse += string(text[i])
-	}
-
-	if text == textReverse {
-		fmt.Println("Es palindromo")
-	} else {
-		fmt.Println("No es un palindromo")
-	}
-}
+import "fmt"
 
 func main() {
-	slice := []string{"hola", "que", "hace"}
+	// Array (Son inmutables)
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-	for i := range slice {
-		fmt.Println(i)
-	}
+	// Slice (Se puede modificar)
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-	isPalindrome("Amor a Roma")
+	// Métodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
+
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	// Append nueva list
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
