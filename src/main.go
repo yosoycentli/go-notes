6e75f234eeb0aed80a975b90b61011d96c1ee9ab
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2022-02-18 16:42:17 -0600

    Primer acercamiento (no el mejor) a las goroutines en Go

--- src/main.go
+++ src/main.go
@@ -1,42 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-type figuras2D interface {
-	area() float64
-}
+func say(text string, wg *sync.WaitGroup) {
 
-type cuadrado struct {
-	base float64
-}
+	defer wg.Done()
 
-type rectangulo struct {
-	base   float64
-	altura float64
-}
-
-func (c cuadrado) area() float64 {
-	return c.base * c.base
-}
-
-func (r rectangulo) area() float64 {
-	return r.base * r.altura
-}
-
-func calcular(f figuras2D) {
-	fmt.Println("Area:", f.area())
+	fmt.Println(text)
 }
 
 func main() {
-	myCuadrado := cuadrado{base: 2}
-	myRectangulo := rectangulo{base: 2, altura: 4}
+	var wg sync.WaitGroup
 
-	calcular(myCuadrado)
-	calcular(myRectangulo)
+	fmt.Println("Hello")
+	wg.Add(1)
 
-	// Lista interfaces
-	myInterface := []interface{}{"Hola", 12, 4.90}
-	fmt.Println(myInterface...)
+	go say("world", &wg)
+
+	wg.Wait()
+
+	go func(text string) {
+		fmt.Println(text)
+	}("Adios")
+
+	time.Sleep(time.Second * 1)
 
 }
 
