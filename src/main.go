bf15e1e8ca16cb5deb9c41ac4971411cb27cfef8
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2022-02-17 18:27:51 -0600

    Como funcionan los arrays y los slices

--- src/main.go
+++ src/main.go
@@ -1,35 +1,37 @@
+2.
+
+
+
+
 package main
 
 import "fmt"
 
 func main() {
+    // Array (Son inmutables)
+    var array [4]int
+    array[0] = 1
+    array[1] = 2
+    fmt.Println(array, len(array), cap(array))
 
-	// For condicinal
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
-	}
+    // Slice (Se puede modificar)
+    slice := []int{0, 1, 2, 3, 4, 5, 6}
+    fmt.Println(slice, len(slice), cap(slice))
 
-	// For while
-	counter := 0
-	for counter < 10 {
-		fmt.Println(counter)
-		counter++
-	}
+    // Métodos en el slice
+    fmt.Println(slice[0])
+    fmt.Println(slice[:3])
+    fmt.Println(slice[2:4])
+    fmt.Println(slice[4:])
 
-	// For forever
-	// counterForever := 0
-	// for {
-	// 	fmt.Println(counterForever)
-	// 	counterForever++
-	// }
+    // Append
+    slice = append(slice, 7)
+    fmt.Println(slice)
 
-	// Reverse for
-	reverseCount := 10
-	for reverseCount >= 0 {
-		fmt.Println(reverseCount)
-		reverseCount--
-	}
-
+    // Append nueva list
+    newSlice := []int{8, 9, 10}
+    slice = append(slice, newSlice...)
+    fmt.Println(slice)
 }
 
 // To ejecute this file you need to compile it in the terminal, we have two options:
