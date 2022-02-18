24fa351995fd0e7e7126d5763687a2304e85dd63
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2022-02-17 18:46:47 -0600

    Como funciona un struct para generar una clase en Go

--- src/main.go
+++ src/main.go
@@ -2,22 +2,20 @@ package main
 
 import "fmt"
 
+type car struct {
+	brand string
+	year  int
+}
+
 func main() {
-	m := make(map[string]int)
+	myCar := car{brand: "Ford", year: 2020}
+	fmt.Println(myCar)
 
-	m["Jose"] = 14
-	m["Pepito"] = 20
+	// Otra manera
+	var otherCar car
+	otherCar.brand = "Ferrari"
+	fmt.Println(otherCar)
 
-	fmt.Println(m)
-
-	// Recorrer map
-	for i, v := range m {
-		fmt.Println(i, v)
-	}
-
-	// Encontrar valor
-	value, ok := m["Jose"]
-	fmt.Println(value, ok)
 }
 
 // To ejecute this file you need to compile it in the terminal, we have two options:
