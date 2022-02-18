4760c8c149203c8ca1afb5ca697e04bb004530a2
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2022-02-17 18:15:43 -0600

    Cuando usar un swith con y sin condicion especificada

--- src/main.go
+++ src/main.go
@@ -3,30 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	// Defer
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
 
-	// Continue y break
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	// Condición específica:
+	//  Usar cuando vas a iterar sonbre una sola variable
+	switch modulo := 4 % 2; modulo {
+	case 0:
+		fmt.Println("Es par")
+	default:
+		fmt.Println("Es impar")
+	}
 
-		// Continue
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
-
-		// Break
-		if i == 8 {
-			fmt.Println("Break")
-			break
-		}
+	// Sin condicion:
+	//  Utilizar cuando vas a hacer otra cosa.
+	//  Como si una variable es mayor a otra o similares
+	value := 50
+	switch {
+	case value > 100:
+		fmt.Println("Es mayor a 100")
+	case value < 0:
+		fmt.Println("Es menor a 0")
+	default:
+		fmt.Println("No condicion")
 	}
 }
 
 // To ejecute this file you need to compile it in the terminal, we have two options:
 //  1. the fast: go run src/main.go
 //  2. the "hard" but with better performance:
-// 	2.1 go build src/main.go
-// 	2.2 ./main
+//  2.1 go build src/main.go
+//  2.2 ./main
