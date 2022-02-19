dc50f7e05d604464100ed9a5ae9dc326104500f5
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2022-02-18 21:47:22 -0600

    Como es el uso de range, close y select en un channel

--- src/main.go
+++ src/main.go
@@ -2,18 +2,52 @@ package main
 
 import "fmt"
 
-func say(text string, c chan<- string) {
+func message(text string, c chan string) {
 	c <- text
 }
 
 func main() {
-	c := make(chan string, 1)
+	// el número establece la capacidad máxima de goroutines que soporta el channel " c"
+	// en este caso es 2, pero si no le pones nada se creará la capacidad de forma dinámica
+	c := make(chan string, 2)
+	c <- "Mensaje1"
+	c <- "Mensaje2"
 
-	fmt.Println("Hello")
+	// len nos dice la cantidad de goroutines que hay en un channel (2)
+	// cap nos dice la capacidad de goroutines que soporta el channel (2)
+	fmt.Println(len(c), cap(c))
 
-	go say("Bye", c)
+	// Range y close
 
-	fmt.Println(<-c)
+	// close cierra el canal
+	close(c)
+
+	// este mensaje ya no puede entrar porque el canal se cerró, además de que ya está lleno
+	//c <- "Mensaje3"
+
+	// el for nos ayuda a hacer un recorrido por los mensajes del canal c
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	// Select
+
+	// cuando tenemos varios canales y no tenemos certeza de cual va a responder primero
+	//  usamos select
+
+	// Como no tienen especificado la capacidad, Go la asignará de forma dinñamica
+	email1 := make(chan string)
+	email2 := make(chan string)
+	go message("mensaje1", email1)
+	go message("mensaje2", email2)
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email2", m2)
+		}
+	}
 }
 
 // To ejecute this file you need to compile it in the terminal, we have two options:
