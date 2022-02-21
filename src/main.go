f35b911fb76aa7b1c5b58725bd535b78a707b84a
Centli Allan Garcés Buendia <yosoycentli@gmail.com>
2022-02-21 01:07:10 -0600

    Adicion de un array vacio

--- src/main.go
+++ src/main.go
@@ -1,37 +1,36 @@
-2.
-
-
-
-
 package main
 
 import "fmt"
 
 func main() {
-    // Array (Son inmutables)
-    var array [4]int
-    array[0] = 1
-    array[1] = 2
-    fmt.Println(array, len(array), cap(array))
+	// Array (Son inmutables)
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-    // Slice (Se puede modificar)
-    slice := []int{0, 1, 2, 3, 4, 5, 6}
-    fmt.Println(slice, len(slice), cap(slice))
+	// Slice (Se puede modificar)
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-    // Métodos en el slice
-    fmt.Println(slice[0])
-    fmt.Println(slice[:3])
-    fmt.Println(slice[2:4])
-    fmt.Println(slice[4:])
+	// Métodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
 
-    // Append
-    slice = append(slice, 7)
-    fmt.Println(slice)
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
 
-    // Append nueva list
-    newSlice := []int{8, 9, 10}
-    slice = append(slice, newSlice...)
-    fmt.Println(slice)
+	// Append nueva list
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
+
+	// Array vacío
+	slice2 := []int{}
+	fmt.Println(slice2)
 }
 
 // To ejecute this file you need to compile it in the terminal, we have two options:
